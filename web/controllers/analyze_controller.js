e7ffea7117f876c8603ecf7764cc21767c22196d
sulted-fish <13818195702@163.com>
2018-08-19 10:42:41 +0800

    补上传

--- web/controllers/analyze_controller.js
+++ web/controllers/analyze_controller.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var filePath = './python/online_analyze.py';
 
+var net = require('net');
+
 var commandConfig = require('../command_config');
 
 var command = commandConfig.pythonCommand + filePath;
@@ -22,54 +24,32 @@ exports.doTextAnalyze = function(req,res){
         return;
     }
 
-    fs.writeFile('./python/text.txt',req.body.text,function(err){
-        if(err) {
-            console.error(err);
-            res.send({
-                code:'500',
-                msg:"解析失败 请重试"
-            }).end();
-            return;
-        } else {
-            exec(command,function(err,stdout,stderr){
-                    if(err)
-                    {
-                        console.log('stderr',err);
-                        res.send({
-                            code:'500',
-                            msg:"解析失败 请重试"
-                        }).end();
-                        return;
-                    }
-                    if(stdout)
-                    {
+    var client = net.Socket();
+    
+    client.connect(8484, '127.0.0.1', function(){
+        client.write(req.body.text);
+    });
 
-                        // console.log(stdout)
-                        // console.log(typeof(stdout));
-
-                        var obj = eval('(' + stdout+ ')');
-
-                        var classificationDict = {
-                            "study":"学习学术",
-                            "activity":"社团活动",
-                            "entrance":"招生考试",
-                            "social":"社会新闻"
-                        };
-                        var sentimentDict = {
-                            "1":"正面情感",
-                            "0":"中性情感",
-                            "-1":"负面情感"
-                        };
-                        resObj = {};
-                        resObj.sentiment = sentimentDict[obj.sentiment];
-                        resObj.classification = classificationDict[obj.classification];
-                        res.send({
-                            code:'200',
-                            result:resObj
-                        }).end();
-                    }
-            })
-        }
+    client.on('data', function(data){
+        var obj = JSON.parse(data)
+        var classificationDict = {
+            "study":"学习学术",
+            "activity":"社团活动",
+            "entrance":"招生考试",
+            "social":"社会新闻"
+        };
+        var sentimentDict = {
+            "1":"正面情感",
+            "0":"中性情感",
+            "-1":"负面情感"
+        };
+        resObj = {};
+        resObj.sentiment = sentimentDict[obj.sentiment];
+        resObj.classification = classificationDict[obj.classification];
+        res.send({
+            code:'200',
+            result:resObj
+        }).end();
     });
     
 };
